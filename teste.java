51b54d8399080476dcb01d8e64c864212ce040aa
christiane gozza <christiane_natacha@hotmail.com>
2021-08-14 01:40:51 -0300

    Primeiro codigo java

---
+++ teste.java
@@ -0,0 +1,10 @@
+package br.com.dio;
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+        int a =5;
+        int b =3;
+
+        System.out.println("hello World! " + (a+b));
+    }/**/
+}
